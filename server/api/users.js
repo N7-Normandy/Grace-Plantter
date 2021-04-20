60087e9e19b2f950c63167830ba570d39153242d
Laura Van Dyke <laura.e.vandyke@gmail.com>
2021-04-20 18:06:05 -0400

    comment out test route

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,8 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
+const router = require('express').Router();
+const {
+  models: { User },
+} = require('../db');
+module.exports = router;
 
 router.get('/', async (req, res, next) => {
   try {
@@ -8,19 +10,19 @@ router.get('/', async (req, res, next) => {
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.json(users)
+      attributes: ['id', 'username'],
+    });
+    res.json(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-router.post('/', async (req, res, next) => {
-  try {
-    const user = await User.create({req.body})
-    res.json(user);
-  } catch (err) {
-    next(err)
-  }
-})
+// router.post('/', async (req, res, next) => {
+//   try {
+//     const user = await User.create({req.body})
+//     res.json(user);
+//   } catch (err) {
+//     next(err)
+//   }
+// })
