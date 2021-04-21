62b24365f6e85d635bc19ce69ad4d1c5846e7ed0
kaseyharvey98 <kaseyyharveyy@gmail.com>
2021-04-21 11:04:52 -0500

    Updated cart get and set functions in User model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -8,35 +8,44 @@ const axios = require('axios');
 const SALT_ROUNDS = 5;
 
 const User = db.define('user', {
-  email: {
-    type: Sequelize.STRING,
-    unique: true,
-    allowNull: false,
-    validate: {
-      isEmail: true,
-    },
-  },
-  password: {
-    type: Sequelize.STRING,
-  },
-  isAdmin: {
-    type: Sequelize.BOOLEAN,
-    defaultValue: false,
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  shippingAddress: {
-    type: Sequelize.STRING,
-  },
-  billingAddress: {
-    type: Sequelize.STRING,
-  },
-  cart: {
-    type: Sequelize.ARRAY(Sequelize.JSONB),
-    defaultValue: [],
-  },
+	email: {
+		type: Sequelize.STRING,
+		unique: true,
+		allowNull: false,
+		validate: {
+			isEmail: true,
+		},
+	},
+	password: {
+		type: Sequelize.STRING,
+	},
+	isAdmin: {
+		type: Sequelize.BOOLEAN,
+		defaultValue: false,
+	},
+	name: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	shippingAddress: {
+		type: Sequelize.STRING,
+	},
+	billingAddress: {
+		type: Sequelize.STRING,
+	},
+	cart: {
+		type: Sequelize.ARRAY(Sequelize.JSONB),
+		defaultValue: [],
+		get() {
+			return this.getDataValue('cart').map(item => JSON.parse(item));
+		},
+		set(val) {
+			return this.setDataValue(
+				'cart',
+				val.map(item => JSON.stringify(item))
+			);
+		},
+	},
 });
 
 /*
@@ -66,54 +75,54 @@ module.exports = User;
  * instanceMethods
  */
 User.prototype.correctPassword = function (candidatePwd) {
-  // we need to compare the plain version to an encrypted version of the password
-  return bcrypt.compare(candidatePwd, this.password);
+	// we need to compare the plain version to an encrypted version of the password
+	return bcrypt.compare(candidatePwd, this.password);
 };
 
 User.prototype.generateToken = function () {
-  return jwt.sign({ id: this.id }, process.env.JWT);
+	return jwt.sign({id: this.id}, process.env.JWT);
 };
 
 /**
  * classMethods
  */
-User.authenticate = async function ({ email, password }) {
-  const user = await this.findOne({ where: { email } });
-  if (!user || !(await user.correctPassword(password))) {
-    const error = Error('Incorrect email/password');
-    error.status = 401;
-    throw error;
-  }
-  return user.generateToken();
+User.authenticate = async function ({email, password}) {
+	const user = await this.findOne({where: {email}});
+	if (!user || !(await user.correctPassword(password))) {
+		const error = Error('Incorrect email/password');
+		error.status = 401;
+		throw error;
+	}
+	return user.generateToken();
 };
 
 User.findByToken = async function (token) {
-  try {
-    const { id } = await jwt.verify(token, process.env.JWT);
-    const user = User.findByPk(id);
-    if (!user) {
-      throw 'nooo';
-    }
-    return user;
-  } catch (ex) {
-    const error = Error('bad token');
-    error.status = 401;
-    throw error;
-  }
+	try {
+		const {id} = await jwt.verify(token, process.env.JWT);
+		const user = User.findByPk(id);
+		if (!user) {
+			throw 'nooo';
+		}
+		return user;
+	} catch (ex) {
+		const error = Error('bad token');
+		error.status = 401;
+		throw error;
+	}
 };
 
 /**
  * hooks
  */
-const hashPassword = async (user) => {
-  // in case the password has been changed, we want to encrypt it with bcrypt
-  if (user.changed('password')) {
-    user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
-  }
+const hashPassword = async user => {
+	// in case the password has been changed, we want to encrypt it with bcrypt
+	if (user.changed('password')) {
+		user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
+	}
 };
 
 User.beforeCreate(hashPassword);
 User.beforeUpdate(hashPassword);
-User.beforeBulkCreate((users) => {
-  users.forEach(hashPassword);
+User.beforeBulkCreate(users => {
+	users.forEach(hashPassword);
 });
