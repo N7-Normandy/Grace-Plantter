d7837b9db674b6b72284115ead962cb309aa3552
Sarah Zerod <szerod@gmail.com>
2021-04-21 13:46:21 -0400

    feat and style: add non-functional search bar and button with styling to stick to wireframe

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -8,24 +8,30 @@ import { logout } from '../store';
 const Navbar = ({ handleClick, isLoggedIn, cart }) => (
   <header className="flex-row">
     <h1>Grace Plantter</h1>
-    <nav>
-      <Link to="/home">Home</Link>
-      <Link to="/cart">Cart ({cart.length} items)</Link>
-      {isLoggedIn ? (
-        <>
-          <Link to="/account">Account</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </>
-      ) : (
-        <>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </>
-      )}
-    </nav>
+    <div className="flex-column nav-wrapper">
+      <nav>
+        <Link to="/home">Home</Link>
+        <Link to="/cart">Cart ({cart.length} items)</Link>
+        {isLoggedIn ? (
+          <>
+            <Link to="/account">Account</Link>
+            <a href="#" onClick={handleClick}>
+              Logout
+            </a>
+          </>
+        ) : (
+          <>
+            {/* The navbar will show these links before you log in */}
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+          </>
+        )}
+      </nav>
+      <form>
+        <input type="text" placeholder="Search..." />
+        <button type="button">Search</button>
+      </form>
+    </div>
   </header>
 );
 
