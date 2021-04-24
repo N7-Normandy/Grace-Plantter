adfb6a0374290015b1c41fa8518f443759bbf4a5
kaseyharvey98 <kaseyyharveyy@gmail.com>
2021-04-24 15:58:01 -0500

    Connected fetchart to Db

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { LOG_OUT } from './auth';
+import {LOG_OUT} from './auth';
 //Action types
 const GET_CART = 'GET_CART';
 const UPDATE_CART = 'UPDATE_CART';
@@ -8,82 +8,81 @@ const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 
 //Actions creators
 const getCart = cart => ({
-  type: GET_CART,
-  cart,
+	type: GET_CART,
+	cart,
 });
 const updateCart = cart => ({
-  type: UPDATE_CART,
-  cart,
+	type: UPDATE_CART,
+	cart,
 });
 const checkoutCart = cart => ({
-  type: CHECKOUT_CART,
-  cart,
+	type: CHECKOUT_CART,
+	cart,
 });
 const removeFromCart = cart => ({
-  type: REMOVE_FROM_CART,
-  cart,
+	type: REMOVE_FROM_CART,
+	cart,
 });
 
 //Thunk creators
-export const fetchCart = () => {
-  return async dispatch => {
-		const {data: userCart} = await axios.get ('/api/cart');
-		console.log("inside fetchCart", userCart);
-    dispatch (getCart (userCart));
-  };
+export const fetchCart = userId => {
+	return async dispatch => {
+		// console.log(userId);
+		const {data: userCart} = await axios.get(`api/cart/${userId}`);
+		console.log('inside fetchCart', userCart);
+		dispatch(getCart(userCart));
+	};
 };
 export const getUpdateCart = cart => {
-  return async dispatch => {
-    const {data: userCart} = await axios.put ('/api/cart', cart);
-    dispatch (updateCart (userCart));
-  };
+	return async dispatch => {
+		const {data: userCart} = await axios.put('/api/cart', cart);
+		dispatch(updateCart(userCart));
+	};
 };
 export const getCheckoutCart = cart => {
-  return async dispatch => {
-    const {data: order} = await axios.post ('/api/cart', cart);
-    dispatch (checkoutCart (order));
-  };
+	return async dispatch => {
+		const {data: order} = await axios.post('/api/cart', cart);
+		dispatch(checkoutCart(order));
+	};
 };
 
 export const getRemoveFromCart = id => async dispatch => {
-  try {
-    console.log ('inside getRemoveFromCart in redux', id);
+	try {
 		const userCart = await axios.put('/api/cart/remove', {id});
-		console.log("DATA-->",userCart.data);
-    dispatch (removeFromCart (userCart.data));
-  } catch (error) {
-    console.log (error);
-  }
+		dispatch(removeFromCart(userCart.data));
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export const addItemsToCart = (userId, items) => {
-  return async (dispatch) => {
-    try {
-      await axios.put(`/api/users/${userId}/orders`, items);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	return async dispatch => {
+		try {
+			await axios.put(`/api/users/${userId}/orders`, items);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 };
 
 // Reducer
 export default (state = [], action) => {
-  switch (action.type) {
-    case LOG_OUT:
-      return [];
-    case GET_CART:
-      return action.cart;
-    case UPDATE_CART:
-      return state.map (cart => {
-        console.log (cart);
-        return cart.id === action.cart.id ? action.cart : cart;
-      });
-    case CHECKOUT_CART:
-      return action.cart;
-    case REMOVE_FROM_CART:
-      console.log ('inside REMOVE_FROM_CART reducer', action.cart);
-      return action.cart;
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case LOG_OUT:
+			return [];
+		case GET_CART:
+			return action.cart;
+		case UPDATE_CART:
+			return state.map(cart => {
+				console.log(cart);
+				return cart.id === action.cart.id ? action.cart : cart;
+			});
+		case CHECKOUT_CART:
+			return action.cart;
+		case REMOVE_FROM_CART:
+			console.log('inside REMOVE_FROM_CART reducer', action.cart);
+			return action.cart;
+		default:
+			return state;
+	}
 };
--- server/api/cart.js
+++ server/api/cart.js
@@ -2,38 +2,22 @@ const router = require('express').Router();
 const {
 	models: {User, Plant, Order},
 } = require('../db');
+
+module.exports = router;
+
 //get cart
-let myCart = [
-	{
-		plant: {
-			id: 1,
-			name: 'Orchid',
-			price: 120,
-			imageUrl:
-				'https://images.thdstatic.com/productImages/e154a248-ac84-46bf-a1a9-65724e122958/svn/endless-summer-bushes-14750-64_400.jpg',
-		},
-		quantity: 2,
-	},
-	{
-		plant: {
-			id: 2,
-			name: 'Peony',
-			price: 18.95,
-			imageUrl:
-				' https://images.thdstatic.com/productImages/b33f34d0-8f91-4e49-a1d8-2eaab0012500/svn/proven-winners-bushes-14766-64_100.jpg',
-		},
-		quantity: 12,
-	},
-];
-router.get('/', async (req, res, next) => {
+router.get('/:userId', async (req, res, next) => {
 	try {
-		// const user = await User.byToken(req.headers.authorization);
-		// const myCart = []
-		// user.cart.forEach(plant => {
-		// 	const plant = await Plant.findByPk(plant);
-		// 	myCart.push(plant)
-		// });
-		res.json(myCart);
+		let cart = await Order.findOne({
+			where: {
+				userId: req.params.userId,
+				status: 'cart',
+			},
+			include: {
+				model: Plant,
+			},
+		});
+		res.json(cart);
 	} catch (err) {
 		next(err);
 	}
@@ -90,15 +74,13 @@ router.post('/', async (req, res, next) => {
 
 router.put('/remove', async (req, res, next) => {
 	try {
-		const id = req.body.id
+		const id = req.body.id;
 		myCart = myCart.filter(item => {
 			return id !== item.plant.id;
-		})
-		console.log("sending response back", myCart)
+		});
+		console.log('sending response back', myCart);
 		res.json(myCart);
 	} catch (err) {
 		next(err);
 	}
 });
-
-module.exports = router;
