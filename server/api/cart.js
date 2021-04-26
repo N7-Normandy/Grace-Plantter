1491bd0ba793c2f537bd68632b9e21350f0b809e
Sarah Zerod <szerod@gmail.com>
2021-04-26 18:12:04 -0400

    Real merge conflict fix

--- server/api/cart.js
+++ server/api/cart.js
@@ -7,7 +7,6 @@ module.exports = router;
 
 // get cart
 router.get('/:userId', async (req, res, next) => {
-<<<<<<< HEAD
   try {
     let cart = await Order.findOne({
       where: {
@@ -16,7 +15,7 @@ router.get('/:userId', async (req, res, next) => {
       },
       include: {
         model: Plant,
-        order: [['plants.orderProducts.createdAt', 'ASC']],
+        order: [['plants.name', 'ASC']],
       },
     });
     res.json(cart);
@@ -35,7 +34,7 @@ router.put('/:userId', async (req, res, next) => {
       },
       include: {
         model: Plant,
-        order: [['plants.orderProducts.createdAt', 'ASC']],
+        order: [['plants.name', 'ASC']],
       },
     });
     await cart.addPlant(changedPlant, {
@@ -57,7 +56,7 @@ router.put('/:userId/remove', async (req, res, next) => {
       },
       include: {
         model: Plant,
-        order: [['plants.orderProducts.createdAt', 'ASC']],
+        order: [['plants.name', 'ASC']],
       },
     });
     await cart.removePlant(oldPlant);
@@ -68,173 +67,57 @@ router.put('/:userId/remove', async (req, res, next) => {
   }
 });
 //checkout
-router.post('/:userId', async (req, res, next) => {
+router.post('/:userId/checkout', async (req, res, next) => {
   try {
-    let accepted = true;
-    // const user = await User.byToken(req.headers.authorization);
+    //total payment
+
+    //
     //check if quantity is available
-    let cart = await Order.findOne({
+    const cart = await Order.findOne({
       where: {
         userId: req.params.userId,
         status: 'cart',
       },
       include: {
         model: Plant,
-        order: [['plants.orderProducts.createdAt', 'ASC']],
+        order: [['plants.name', 'ASC']],
       },
     });
 
-    let plants = [];
+    const total =
+      cart.plants.reduce((acc, curr) => {
+        return acc + curr.price * curr.orderProducts.quantity;
+      }, 0) / 100;
 
-    for (let index = 0; index < cart.length; index++) {
-      // search through every plant
-      const plant = await Plant.findByPk(cart[index].plant.id);
-      // if cart has more than databse, accepted = false
-      if (cart[index].plant.quantity > plant.quantity) {
-        accepted = false;
+    let inStock = true;
+    const plants = [];
+    for (let index = 0; index < cart.plants.length; index++) {
+      //search through every plant
+      const plant = await Plant.findByPk(cart.plants[index].id);
+      //if cart has more than databse, inStock = false
+      if (cart.plants[index].orderProducts.quantity > plant.quantity) {
+        inStock = false;
         break;
       } else {
         plants.push(plant);
-        // reduce plant number in db
-        // const newQuantity = plant.quantity - cart[index].plant.quantity;
-        // await plant.update({ quantity: newQuantity });
       }
     }
+    console.log('inStock', inStock);
     // if everything is acccepted, empty cart and update paste orders
-    if (accepted) {
-      Promise.all(
+    if (inStock) {
+      await Promise.all(
         plants.map((pla, idx) => {
           const newQuantity =
             pla.quantity - cart.plants[idx].orderProducts.quantity;
           return pla.update({ quantity: newQuantity });
         })
       );
-
-      await user.update({ cart: [] });
-      res.json(
-        await Order.create({
-          plantsBought: cart,
-          totalPrice: req.body.totalPrice,
-          paymentType: req.body.paymentType,
-        })
-      );
+      await cart.update({ status: 'purchased', totalPrice: total });
+      res.sendStatus('200');
+    } else {
+      res.sendStatus('555');
     }
   } catch (err) {
     next(err);
   }
-=======
-	try {
-		let cart = await Order.findOne({
-			where: {
-				userId: req.params.userId,
-				status: 'cart',
-			},
-			include: {
-				model: Plant,
-				order: [['plants.name', 'ASC']],
-			},
-		});
-		res.json(cart);
-	} catch (err) {
-		next(err);
-	}
-});
-// update cart
-router.put('/:userId', async (req, res, next) => {
-	try {
-		const changedPlant = await Plant.findByPk(req.body.plant.id);
-		const cart = await Order.findOne({
-			where: {
-				userId: req.params.userId,
-				status: 'cart',
-			},
-			include: {
-				model: Plant,
-				order: [['plants.name', 'ASC']],
-			},
-		});
-		await cart.addPlant(changedPlant, {
-			through: {quantity: req.body.plant.orderProducts.quantity},
-		});
-		await cart.reload();
-		res.json(cart);
-	} catch (err) {
-		next(err);
-	}
-});
-router.put('/:userId/remove', async (req, res, next) => {
-	try {
-		const oldPlant = await Plant.findByPk(req.body.plantId);
-		const cart = await Order.findOne({
-			where: {
-				userId: req.params.userId,
-				status: 'cart',
-			},
-			include: {
-				model: Plant,
-				order: [['plants.name', 'ASC']],
-			},
-		});
-		await cart.removePlant(oldPlant);
-		await cart.reload();
-		res.json(cart);
-	} catch (err) {
-		next(err);
-	}
-});
-//checkout
-router.post('/:userId/checkout', async (req, res, next) => {
-	try {
-		//total payment
-
-		//
-		//check if quantity is available
-		const cart = await Order.findOne({
-			where: {
-				userId: req.params.userId,
-				status: 'cart',
-			},
-			include: {
-				model: Plant,
-				order: [['plants.name', 'ASC']],
-			},
-		});
-
-		const total =
-			cart.plants.reduce((acc, curr) => {
-				return acc + curr.price * curr.orderProducts.quantity;
-			}, 0) / 100;
-
-		let inStock = true;
-		const plants = [];
-		for (let index = 0; index < cart.plants.length; index++) {
-			//search through every plant
-			const plant = await Plant.findByPk(cart.plants[index].id);
-			//if cart has more than databse, inStock = false
-			if (cart.plants[index].orderProducts.quantity > plant.quantity) {
-				inStock = false;
-				break;
-			} else {
-				plants.push(plant);
-			}
-		}
-		console.log('inStock', inStock);
-		// if everything is acccepted, empty cart and update paste orders
-		if (inStock) {
-			await Promise.all(
-				plants.map((pla, idx) => {
-					const newQuantity =
-						pla.quantity - cart.plants[idx].orderProducts.quantity;
-					return pla.update({quantity: newQuantity});
-				})
-			);
-			await cart.update({status: 'purchased', totalPrice: total});
-			res.sendStatus('200');
-		} else {
-			res.sendStatus('555');
-		}
-	} catch (err) {
-		next(err);
-	}
->>>>>>> main
 });
