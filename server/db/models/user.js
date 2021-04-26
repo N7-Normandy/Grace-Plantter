c451f4ad8216b6c753160694d7fb0349e9432156
Sarah Zerod <szerod@gmail.com>
2021-04-26 18:16:33 -0400

    Edit User Model to allow all fields to be null to hold express sessions; Edit AuthForm to make those fields required on client-side instead

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -11,7 +11,6 @@ const User = db.define('user', {
   email: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false,
     validate: {
       isEmail: true,
     },
@@ -25,7 +24,6 @@ const User = db.define('user', {
   },
   name: {
     type: Sequelize.STRING,
-    allowNull: false,
   },
   shippingAddress: {
     type: Sequelize.STRING,
@@ -41,12 +39,12 @@ module.exports = User;
  * instanceMethods
  */
 User.prototype.correctPassword = function (candidatePwd) {
-	// we need to compare the plain version to an encrypted version of the password
-	return bcrypt.compare(candidatePwd, this.password);
+  // we need to compare the plain version to an encrypted version of the password
+  return bcrypt.compare(candidatePwd, this.password);
 };
 
 User.prototype.generateToken = function () {
-	return jwt.sign({id: this.id}, process.env.JWT);
+  return jwt.sign({ id: this.id }, process.env.JWT);
 };
 
 User.prototype.addToCart = function (item) {
@@ -57,43 +55,43 @@ User.prototype.addToCart = function (item) {
 /**
  * classMethods
  */
-User.authenticate = async function ({email, password}) {
-	const user = await this.findOne({where: {email}});
-	if (!user || !(await user.correctPassword(password))) {
-		const error = Error('Incorrect email/password');
-		error.status = 401;
-		throw error;
-	}
-	return user.generateToken();
+User.authenticate = async function ({ email, password }) {
+  const user = await this.findOne({ where: { email } });
+  if (!user || !(await user.correctPassword(password))) {
+    const error = Error('Incorrect email/password');
+    error.status = 401;
+    throw error;
+  }
+  return user.generateToken();
 };
 
 User.findByToken = async function (token) {
-	try {
-		const {id} = await jwt.verify(token, process.env.JWT);
-		const user = User.findByPk(id);
-		if (!user) {
-			throw 'nooo';
-		}
-		return user;
-	} catch (ex) {
-		const error = Error('bad token');
-		error.status = 401;
-		throw error;
-	}
+  try {
+    const { id } = await jwt.verify(token, process.env.JWT);
+    const user = User.findByPk(id);
+    if (!user) {
+      throw 'nooo';
+    }
+    return user;
+  } catch (ex) {
+    const error = Error('bad token');
+    error.status = 401;
+    throw error;
+  }
 };
 
 /**
  * hooks
  */
-const hashPassword = async user => {
-	// in case the password has been changed, we want to encrypt it with bcrypt
-	if (user.changed('password')) {
-		user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
-	}
+const hashPassword = async (user) => {
+  // in case the password has been changed, we want to encrypt it with bcrypt
+  if (user.changed('password')) {
+    user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
+  }
 };
 
 User.beforeCreate(hashPassword);
 User.beforeUpdate(hashPassword);
-User.beforeBulkCreate(users => {
-	users.forEach(hashPassword);
+User.beforeBulkCreate((users) => {
+  users.forEach(hashPassword);
 });
